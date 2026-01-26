1174772922e8f14dd9804ba1a20012434446e14c
ujjwalJh <who.ujjwaljhariya@gmail.com>
2026-01-26 16:37:52 +0530

    third risky one

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
-import "./register.css";
+import "./Register.css";
 
 export default function Register() {
   const navigate = useNavigate();
